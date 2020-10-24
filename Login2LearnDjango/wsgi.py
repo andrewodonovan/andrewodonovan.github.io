8ca49926c1b10bed51fec859d744a36399f421f6
Andrew O' Donovan <36671447+andrewodonovan@users.noreply.github.com>
2020-10-24 22:02:24 +0100

    Fixed WSGI

--- Login2LearnDjango/wsgi.py
+++ Login2LearnDjango/wsgi.py
@@ -9,9 +9,7 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Login2LearnDjango.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
