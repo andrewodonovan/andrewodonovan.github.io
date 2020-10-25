baa81ebcdb08e4ee1792a64ff165507a4abf9b3e
Andrew O' Donovan <36671447+andrewodonovan@users.noreply.github.com>
2020-10-25 15:30:50 +0000

    Split About Us

--- about/urls.py
+++ about/urls.py
@@ -1,7 +1,10 @@
 # home/urls.py
 from django.urls import path
-from .views import About
+from .views import About, Testimonial, Faq, Contact
 
 urlpatterns = [
     path('', About.as_view(), name='about'),
+    path('/testimonial', Testimonial.as_view(), name='testimonial'),
+    path('/faq', Faq.as_view(), name='faq'),
+    path('contact', Contact.as_view(), name='contact'),
 ]
--- about/views.py
+++ about/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
 # home/views.py
 from django.views.generic import TemplateView
 
 
 class About(TemplateView):
     template_name = 'about.html'
+
+
+class Testimonial(TemplateView):
+    template_name = 'testimonial.html'
+
+
+class Faq(TemplateView):
+    template_name = 'faq.html'
+
+
+class Contact(TemplateView):
+    template_name = 'contact.html'
