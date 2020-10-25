ad9e5033513daaa3dae095329f80233046244ed3
Andrew O' Donovan <36671447+andrewodonovan@users.noreply.github.com>
2020-10-25 15:38:08 +0000

    fixed linking

--- about/urls.py
+++ about/urls.py
@@ -4,7 +4,7 @@ from .views import About, Testimonial, Faq, Contact
 
 urlpatterns = [
     path('', About.as_view(), name='about'),
-    path('/testimonial', Testimonial.as_view(), name='testimonial'),
-    path('/faq', Faq.as_view(), name='faq'),
+    path('testimonial', Testimonial.as_view(), name='testimonial'),
+    path('faq', Faq.as_view(), name='faq'),
     path('contact', Contact.as_view(), name='contact'),
 ]
